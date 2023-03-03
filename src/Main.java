4f8cb8836ef3851093290c46c900cc1905a5dacc
Filipe Mota <filipe.mota@gmail.com>
2023-03-03 10:05:13 -0300

    Versão final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -12,7 +14,7 @@ public class Main {
         curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
-        curso2.setTitulo("Curso Java");
+        curso2.setTitulo("Curso JavaScript");
         curso2.setDescricao("Descrição Curso java");
         curso2.setCargaHoraria(4);
 
@@ -22,7 +24,33 @@ public class Main {
         mentoria.setDescricao("Descrição mentoria Java");
         mentoria.setData(LocalDate.now());
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descricao Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
 
+        Dev dev1 = new Dev();
+        dev1.setNome("Mario");
+        dev1.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos: " + " " +dev1.getNome() + " " + dev1.getConteudosInscritos());
+        dev1.progredir();
+        dev1.progredir();
+        System.out.println("- Progreção -");
+        System.out.println("Conteudos inscritos: " + " " +dev1.getNome() + " " + dev1.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos: " + " " +dev1.getNome() + " " + dev1.getConteudosConcluidos());
+        System.out.println("XP: " + dev1.calcularXP());
 
+        System.out.println("----------------------");
+        Dev dev2 = new Dev();
+        dev2.setNome("Jose");
+        dev2.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos: " + " " +dev2.getNome() + " " + dev2.getConteudosInscritos());
+        dev2.progredir();
+        System.out.println("- Progreção -");
+        System.out.println("Conteudos inscritos: " + " " +dev2.getNome() + " " + dev2.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos: " + " " +dev2.getNome() + " " + dev2.getConteudosConcluidos());
+        System.out.println("XP: " + dev2.calcularXP());
     }
 }
